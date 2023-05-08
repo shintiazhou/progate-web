c48820d5d884ca669f84d592991805bec5173956
shintia <shintiazhouu28@gmail.com>
2023-05-08 10:09:20 +0700

    fix button color

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -5,13 +5,13 @@
 
   const styles = {
     primary: "bg-primary text-dark",
-    secondary: "bg-secondary",
+    secondary: "bg-secondary text-white ",
   };
   bgColorClass = styles[color] || "bg-gray-200 text-dark";
 </script>
 
 <button
-  class={`transition-all hover:scale-105 text-white px-4 py-2 rounded ${bgColorClass} ${customClass}`}
+  class={`transition-all hover:scale-105 px-4 py-2 rounded ${bgColorClass} ${customClass}`}
 >
   <slot />
 </button>
