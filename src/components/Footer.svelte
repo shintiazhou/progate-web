e1b559346ab556433598d8a629ee5423e47cd593
Dian Anggun Septiani <dian@Dians-MacBook-Air.local>
2023-05-09 08:10:34 +0700

    articles page

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,1 +1,54 @@
-<footer class="border px-10 py-4">This is footer</footer>
+<script>
+  import gojek from "../lib/logos/gojek.png";
+  import FaTwitter from "svelte-icons/fa/FaTwitter.svelte";
+  import FaFacebookF from "svelte-icons/fa/FaFacebookF.svelte";
+  import FaInstagram from "svelte-icons/fa/FaInstagram.svelte";
+</script>
+
+<footer>
+  <div class="border px-10 py-4 flex items-center justify-between flex-wrap">
+    <div class="flex justify-start">
+      <a
+        href="/articles"
+        class="text-gray-400 px-7 py-2 rounded-md text-sm font-medium"
+        >Articles</a
+      >
+      <a
+        href="/forum"
+        class="text-gray-400 px-7 py-2 rounded-md text-sm font-medium">Forum</a
+      >
+      <a
+        href="/pricing"
+        class="text-gray-400 px-7 py-2 rounded-md text-sm font-medium"
+        >Pricing</a
+      >
+      <a
+        href="/contact"
+        class="text-gray-400 px-7 py-2 rounded-md text-sm font-medium"
+        >Contact</a
+      >
+    </div>
+    <div class="flex items-center">
+      <img
+        src={gojek}
+        alt="gojek"
+        class="w-[100px] h-[30px] object-contain mr-6"
+      />
+      <img src={gojek} alt="gojek" class="w-[100px] h-[30px] object-contain" />
+    </div>
+  </div>
+
+  <div class="border px-10 py-1 flex items-center justify-between flex-wrap">
+    <div class="flex justify-start">
+      <p class="text-gray-400 px-7 py-2 rounded-md text-sm font-medium">
+        &copy; 2023 - Progate | All right reserved
+      </p>
+    </div>
+
+    <div class="flex items-center">
+      <div class="mx-5 w-[20px] h-[20px]"><FaTwitter /></div>
+      <div class="mx-5 w-[20px] h-[20px]"><FaFacebookF /></div>
+      <div class="mx-5 w-[20px] h-[20px]"><FaInstagram /></div>
+    </div>
+  </div>
+</footer>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,1 +1,43 @@
-<header class="bg-primary py-4 px-10">This is header</header>
+<script>
+  import gojek from "../lib/logos/gojek.png";
+</script>
+
+<header
+  class="flex items-center justify-between flex-wrap bg-primary py-3 px-5 md:px-10"
+>
+  <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <a href="/">
+      <img src={gojek} alt="gojek" class="w-[100px] h-[30px] object-contain" />
+    </a>
+  </div>
+  <div class="flex justify-center w-full md:w-auto">
+    <a
+      href="/articles"
+      class="text-black hover:bg-gray-700 hover:text-white px-4 md:px-7 py-2 rounded-md text-sm font-medium"
+      >Articles</a
+    >
+    <a
+      href="/forum"
+      class="text-black hover:bg-gray-700 hover:text-white px-4 md:px-7 py-2 rounded-md text-sm font-medium"
+      >Forum</a
+    >
+    <a
+      href="/pricing"
+      class="text-black hover:bg-gray-700 hover:text-white px-4 md:px-7 py-2 rounded-md text-sm font-medium"
+      >Pricing</a
+    >
+    <a
+      href="/contact"
+      class="text-black hover:bg-gray-700 hover:text-white px-4 md:px-7 py-2 rounded-md text-sm font-medium"
+      >Contact</a
+    >
+  </div>
+  <div class="flex items-center">
+    <a
+      href="/signin"
+      class="bg-transparent hover:bg-gray-700 hover:text-white text-black font-medium py-2 px-5 md:px-10 border border-black hover:border-transparent rounded"
+    >
+      Sign In
+    </a>
+  </div>
+</header>
