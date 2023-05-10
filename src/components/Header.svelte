018ba14b0b7f76afd4da196073bd09db03ea1abf
shintia <shintiazhouu28@gmail.com>
2023-05-10 21:10:51 +0700

    update search header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import logo from "../lib/Logo.png";
+  import Sidebar from "./Sidebar.svelte";
   let isLoggedIn = false;
 
   onMount(() => {
@@ -29,40 +30,13 @@
   }
 </script>
 
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<div
-  class={`fixed bg-black/50 inset-0 z-10 transition-all ${
-    isOpen ? "block" : "hidden"
-  }`}
-  on:click={toggleMenu}
-/>
-
+<Sidebar {toggleMenu} {isOpen} />
 <header
   class="z-30 fixed shadow inset-x-0 top-0 flex items-center justify-between flex-wrap bg-primary py-3 px-5 md:px-10"
 >
   <a href="/" class="hidden lg:block">
     <img src={logo} alt="logo" class="w-[100px] h-[30px] object-contain" />
   </a>
-
-  <div
-    class={`sidebar bg-light text-dark flex flex-col p-6 ${
-      isOpen ? " open" : ""
-    }`}
-  >
-    <a href="/" on:click={() => (isOpen = false)}>
-      <img src={logo} alt="logo" class="w-[100px] h-[30px] object-contain" />
-    </a>
-    <div class="flex flex-col gap-4 text-lg mt-10">
-      {#each routes as route}
-        <a
-          on:click={() => (isOpen = false)}
-          href={route.href}
-          class="text-black hover:text-blue-600 rounded-md text-sm font-medium"
-          >{route.title}</a
-        >
-      {/each}
-    </div>
-  </div>
 
   <div class="lg:hidden flex flex-col items-center justify-center">
     <button class="text-dark" on:click={toggleMenu}>
@@ -112,19 +86,3 @@
     </a>
   {/if}
 </header>
-
-<style>
-  .sidebar {
-    position: fixed;
-    top: 0;
-    left: 0;
-    height: 100vh;
-    width: 240px;
-    transform: translateX(-100%);
-    transition: transform 0.3s ease-in-out;
-  }
-
-  .sidebar.open {
-    transform: translateX(0);
-  }
-</style>
